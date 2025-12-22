b7c36a4270a4254d405e9f53d27b3fc13a164889
princentf07 <felix.pakpahan10@gmail.com>
2025-12-22 13:37:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pandas as pd
+import plotly as px
 
-st.write("Hello, *MOK* :smile:")
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 
