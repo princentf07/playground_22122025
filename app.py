2c544e175db7bda9acedfc239a04f91be97ada9b
princentf07 <felix.pakpahan10@gmail.com>
2025-12-22 13:46:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="HOTAWW App",
     page_icon="🫦",
     layout="wide",
     initial_sidebar_state="expanded",
