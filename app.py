2b68a611b0ab5a1a31f506b38d71e4937449f6b9
princentf07 <felix.pakpahan10@gmail.com>
2025-12-22 13:45:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
-    page_icon="🦈",
+    page_icon="🫦🫦",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
